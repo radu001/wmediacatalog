fad2ace7ff20eee7c6ec3ec6b2c855c882c4dbd4
ternovich <ternovich@gmail.com>
2011-02-24 11:05:03 +0000

    Initial preparations for additional filtering of listenings by date interval

--- Modules.Listenings/ViewModels/IListeningsViewModel.cs
+++ Modules.Listenings/ViewModels/IListeningsViewModel.cs
@@ -1,14 +1,17 @@
 ﻿
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using BusinessObjects;
 using Common.Controls.Controls;
 using Common.Entities.Pagination;
 using Microsoft.Practices.Composite.Presentation.Commands;
+using Modules.Listenings.Data;
 namespace Modules.Listenings.ViewModels
 {
     public interface IListeningsViewModel
     {
         ILoadOptions LoadOptions { get; }
+        IEnumerable<IntervalFilterEnum> IntervalFilters { get; }
         ObservableCollection<Listening> ListeningsCollection { get; }
         int ListeningsCount { get; }
         Listening SelectedListening { get; set; }
@@ -20,5 +23,6 @@ namespace Modules.Listenings.ViewModels
         DelegateCommand<object> DisplayListeningCommand { get; }
         DelegateCommand<object> CreateArtistCommand { get; }
         DelegateCommand<object> CreateAlbumCommand { get; }
+        DelegateCommand<object> IntervalFilterChangedCommand { get; }
     }
 }
