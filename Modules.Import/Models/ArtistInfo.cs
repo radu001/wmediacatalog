6fdeb2729fea64812e23070db81df10442e43dbc
ternovich <ternovich@gmail.com>
2011-03-15 20:58:22 +0000

    Minor changes

---
+++ Modules.Import/Models/ArtistInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modules.Import.Models
+{
+    public class ArtistInfo
+    {
+        public string Name { get; set; }
+        public List<AlbumInfo> Albums { get; set; }
+    }
+}
---
+++ Tests.DataLayer/Import/ImportTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MediaCatalog.Tests.Import
+{
+    [TestFixture]
+    public class ImportTests
+    {
+    }
+}
