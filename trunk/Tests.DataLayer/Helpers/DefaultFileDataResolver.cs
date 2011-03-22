b62d12a2a68df112933e9473ed33b5af074dbbf0
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-03-22 19:00:02 +0000

    Extracted file data mechanism resolving

---
+++ trunk/Tests.DataLayer/Helpers/DefaultFileDataResolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace MediaCatalog.Tests.Helpers
+{
+    public class DefaultFileDataResolver<T> : IFileDataResolver<T>
+    {
+        #region IFileDataResolver<T> Members
+
+        public T GetFileData(XElement fileElement)
+        {
+            return default(T);
+        }
+
+        #endregion
+    }
+}
