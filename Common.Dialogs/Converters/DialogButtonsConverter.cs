1c496e7b90d0d0b8bd1e06dd6f0810e0cefcbac7
ternovich <ternovich@gmail.com>
2011-03-24 20:44:09 +0000

    Refactored NotificationDialog to CommonDialog updated issue #9

---
+++ Common.Dialogs/Converters/DialogButtonsConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common.Dialogs.Converters
+{
+    public class DialogButtonsConverter : IValueConverter
+    {
+        public DialogButton ButtonType { get; set; }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DialogButtons buttons = (DialogButtons)value;
+
+            switch (ButtonType)
+            {
+                case DialogButton.Ok:
+                    {
+                        if ((buttons & DialogButtons.Ok) == DialogButtons.Ok)
+                            return Visibility.Visible;
+                        else
+                            return Visibility.Collapsed;
+                    }
+                case DialogButton.Cancel:
+                    {
+                        if ((buttons & DialogButtons.Cancel) == DialogButtons.Cancel)
+                            return Visibility.Visible;
+                        else
+                            return Visibility.Collapsed;
+                    }
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Modules.Notifications/Views/NotificationControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using Modules.Notifications.ViewModels;
+
+namespace Modules.Notifications.Views
+{
+    /// <summary>
+    /// Interaction logic for NotificationControl.xaml
+    /// </summary>
+    public partial class NotificationControl : UserControl
+    {
+        public NotificationControl(INotificationsViewModel viewModel)
+        {
+            InitializeComponent();
+
+            DataContext = viewModel;
+        }
+    }
+}
