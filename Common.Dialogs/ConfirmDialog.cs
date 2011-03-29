7a836d5684230a57908b46abbf2ccc106496ff58
ternovich <ternovich@gmail.com>
2011-03-29 06:01:34 +0000

    resolved issue #9 Refactored all model dialogs so they use common dialog implementation

---
+++ Common.Dialogs/ConfirmDialog.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Windows;
+using Common.Dialogs.Views;
+namespace Common.Dialogs
+{
+    public partial class ConfirmDialog : CommonDialog
+    {
+        #region Dependency properties
+
+        public static readonly DependencyProperty MessageTextProperty =
+            DependencyProperty.RegisterAttached("MessageText", typeof(string), typeof(DialogWindow),
+            new PropertyMetadata(OnMessageTextPropertyChanged));
+
+        public string MessageText
+        {
+            get
+            {
+                return (string)GetValue(MessageTextProperty);
+            }
+            set
+            {
+                SetValue(MessageTextProperty, value);
+            }
+        }
+
+        private static void OnMessageTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ConfirmDialog dialog = d as ConfirmDialog;
+            if (dialog != null)
+            {
+                ConfirmDialogView view = dialog.DialogContent as ConfirmDialogView;
+                if (view != null)
+                {
+                    view.MessageText = e.NewValue != null ? e.NewValue.ToString() : String.Empty;
+                }
+            }
+        }
+
+
+        #endregion
+
+        public ConfirmDialog()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            DialogContent = new ConfirmDialogView();
+
+        }
+    }
+}
