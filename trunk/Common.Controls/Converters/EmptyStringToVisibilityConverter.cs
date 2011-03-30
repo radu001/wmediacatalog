e297b14e4f1b2eb2ab1cdd24fb5e6972ab27469d
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-03-30 06:32:18 +0000

    update issue #4 Implemented passing import result from scan progress viewModel back to import one

--- trunk/Common.Controls/Converters/EmptyStringToVisibilityConverter.cs
+++ trunk/Common.Controls/Converters/EmptyStringToVisibilityConverter.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
-
-namespace Common.Dialogs.Converters
+namespace Common.Controls.Converters
 {
     public class EmptyStringToVisibilityConverter : IValueConverter
     {
--- trunk/Common.Controls/Converters/InverseBoolConverter.cs
+++ trunk/Common.Controls/Converters/InverseBoolConverter.cs
@@ -2,28 +2,20 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Common.Dialogs.Converters
+namespace Common.Controls.Converters
 {
-    public class RatingConverter : IValueConverter
+    public class InverseBoolConverter : IValueConverter
     {
-        const double Max = 1;
-        const double Min = 0;
-        const int ItemsCount = 7;
-
         #region IValueConverter Members
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int ival = (int)value;
-
-            return (double)ival/ItemsCount;
+            return (!(bool)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double dval = (double)value;
-
-            return (int)(dval * ItemsCount);
+            throw new NotImplementedException();
         }
 
         #endregion
