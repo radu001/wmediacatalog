b744d876fc7ab96846628e01ddfa024e5f1aa844
ternovich <ternovich@gmail.com>
2011-03-30 05:36:38 +0000

    update issue #4 Logic and layout update

--- Modules.Import/ViewModels/IImportProgressViewModel.cs
+++ Modules.Import/ViewModels/IImportProgressViewModel.cs
@@ -10,9 +10,13 @@ namespace Modules.Import.ViewModels
         int ScannedFilesCount { get; }
         string ScanPath { get; }
         ObservableCollection<Artist> Artists { get; }
+        bool IsScanning { get; }
+        bool IsPaused { get; }
         double CurrentProgress { get; }
 
         DelegateCommand<object> SelectScanPathCommand { get; }
         DelegateCommand<object> BeginScanCommand { get; }
+        DelegateCommand<object> PauseScanCommand { get; }
+        DelegateCommand<object> CancelScanCommand { get; }
     }
 }
