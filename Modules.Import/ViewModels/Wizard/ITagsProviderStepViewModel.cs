3cd6021497c29a04238458872784933a0f2b59f4
ternovich <ternovich@gmail.com>
2011-04-19 15:59:53 +0000

    Improved import data dialog

--- Modules.Import/ViewModels/Wizard/ITagsProviderStepViewModel.cs
+++ Modules.Import/ViewModels/Wizard/ITagsProviderStepViewModel.cs
@@ -1,7 +1,13 @@
 ﻿
+using System.Collections.Generic;
+using Modules.Import.Model;
+using Modules.Import.ViewModels.Wizard.Common;
 namespace Modules.Import.ViewModels.Wizard
 {
-    public interface ITagsProviderStepViewModel
+    public interface ITagsProviderStepViewModel : IWizardViewModel
     {
+        string Message { get; }
+        IEnumerable<TagsProvider> AvaliableProviders { get; }
+        bool ProviderSelected { get; }
     }
 }
--- Modules.Import/ViewModels/Wizard/TagsProviderStepViewModel.cs
+++ Modules.Import/ViewModels/Wizard/TagsProviderStepViewModel.cs
@@ -1,7 +1,80 @@
 ﻿
+using System.Collections.Generic;
+using Microsoft.Practices.Prism.Events;
+using Modules.Import.Model;
+using Modules.Import.ViewModels.Wizard.Common;
 namespace Modules.Import.ViewModels.Wizard
 {
-    public class TagsProviderStepViewModel : ITagsProviderStepViewModel
+    public class TagsProviderStepViewModel : WizardViewModelBase, ITagsProviderStepViewModel
     {
+        public TagsProviderStepViewModel(IEventAggregator eventAggregator)
+            : base(eventAggregator)
+        {
+            InitAvaliableProviders();
+        }
+
+        #region ITagsProviderStepViewModel Members
+
+        public string Message
+        {
+            get
+            {
+                return @"Please select tags provider to continue";
+            }
+        }
+
+        public IEnumerable<TagsProvider> AvaliableProviders
+        {
+            get
+            {
+                return avaliableProviders;
+            }
+            private set
+            {
+                avaliableProviders = value;
+                NotifyPropertyChanged(() => AvaliableProviders);
+            }
+        }
+
+        public bool ProviderSelected
+        {
+            get
+            {
+                return providerSelected;
+            }
+            private set
+            {
+                providerSelected = value;
+                NotifyPropertyChanged(() => ProviderSelected);
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void InitAvaliableProviders()
+        {
+            avaliableProviders = new List<TagsProvider>()
+            {
+                new TagsProvider()
+                {
+                    Name = "FLAC"
+                },
+                new TagsProvider()
+                {
+                    Name = "MP3"
+                }
+            };
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private bool providerSelected;
+        private IEnumerable<TagsProvider> avaliableProviders;
+
+        #endregion
     }
 }
