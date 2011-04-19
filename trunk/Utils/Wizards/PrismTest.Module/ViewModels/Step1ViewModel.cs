cf8ecb401fbb3eca8fda11cd8280916203b921b7
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-04-19 12:29:56 +0000

    Added wizard shared data to share common data between wizard steps

--- trunk/Utils/Wizards/PrismTest.Module/ViewModels/Step1ViewModel.cs
+++ trunk/Utils/Wizards/PrismTest.Module/ViewModels/Step1ViewModel.cs
@@ -1,12 +1,17 @@
 ﻿
 using Microsoft.Practices.Prism.Events;
+using Microsoft.Practices.Unity;
+using Prism.Wizards;
 namespace PrismTest.Module.ViewModels
 {
     public class Step1ViewModel : ViewModelBase, IStep1ViewModel
     {
-        public Step1ViewModel(IEventAggregator eventAggregator)
+        public Step1ViewModel(IUnityContainer container, IEventAggregator eventAggregator)
             :base(eventAggregator)
         {
+            var data = container.Resolve<IWizardData>();
+            data.SetValue<string>("123123");
         }
+
     }
 }
