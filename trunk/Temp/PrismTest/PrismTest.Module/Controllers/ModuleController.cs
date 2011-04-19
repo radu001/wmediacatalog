82f523851465a67aa875b6a49aa70af1be72f5c2
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-04-19 05:39:42 +0000

    Minor memory leak changes for Prism wizarding

--- trunk/Temp/PrismTest/PrismTest.Module/Controllers/ModuleController.cs
+++ trunk/Temp/PrismTest/PrismTest.Module/Controllers/ModuleController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
-using Prism.Wizards;
-using PrismTest.Module.Events;
-using PrismTest.Module.ViewModels;
-using PrismTest.Module.Views;
 
 namespace PrismTest.Module.Controllers
 {
@@ -12,24 +8,15 @@ namespace PrismTest.Module.Controllers
     {
         public ModuleController(IEventAggregator eventAggregator, IUnityContainer container, IRegionManager regionManager)
         {
-            this.eventAggregator = eventAggregator;
             this.container = container;
-
-            eventAggregator.GetEvent<StartWizardEvent>().Subscribe(OnStartWizardEvent, true);
+            //eventAggregator.GetEvent<StartWizardEvent>().Subscribe(OnStartWizardEvent, true);
         }
 
         private void OnStartWizardEvent(object parameter)
         {
-            var settings = new WizardSettings();
-            settings.AddStep<IInitialViewModel, InitialViewModel, InitialView>(0, "Initial step");
-            settings.AddStep<IStep1ViewModel, Step1ViewModel, Step1View>(1, "Step1");
-            settings.AddStep<IStep2ViewModel, Step2ViewModel, Step2View>(2, "Step2");
-            settings.AddStep<ICompleteViewModel, CompleteViewModel, CompleteView>(3, "Complete wizard");
-            Wizard w = new Wizard(container, settings, "wizard1");
-            w.Start();
+
         }
 
-        private IEventAggregator eventAggregator;
         private IUnityContainer container;
     }
 }
