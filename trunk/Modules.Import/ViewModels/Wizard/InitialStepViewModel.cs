a4888af69dba0cb32b75fadb576a2a8aa5aa4894
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-04-21 05:49:17 +0000

    update issue #23 Import data wizard improvements

--- trunk/Modules.Import/ViewModels/Wizard/InitialStepViewModel.cs
+++ trunk/Modules.Import/ViewModels/Wizard/InitialStepViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Practices.Prism.Events;
+using Microsoft.Practices.Unity;
 using Modules.Import.ViewModels.Wizard.Common;
+using Prism.Wizards.Events;
 
 namespace Modules.Import.ViewModels.Wizard
 {
     public class InitialStepViewModel : WizardViewModelBase, IInitialStepViewModel
     {
-        public InitialStepViewModel(IEventAggregator eventAggregator)
-            :base(eventAggregator)
+        public InitialStepViewModel(IUnityContainer container, IEventAggregator eventAggregator)
+            : base(container, eventAggregator)
         {
+            this.eventAggregator = eventAggregator;
         }
 
         #region IInitialStepViewModel Members
@@ -16,11 +19,18 @@ namespace Modules.Import.ViewModels.Wizard
         {
             get
             {
-                return @"Welcome to import wizard. It will guide you through the process of " + 
+                return @"Welcome to import wizard. It will guide you through the process of " +
                          "importing data from your local storage(s) into media library";
             }
         }
 
         #endregion
+
+        public override void OnContinueCommand(object parameter)
+        {
+            eventAggregator.GetEvent<CompleteWizardStepEvent>().Publish(null);
+        }
+
+        private IEventAggregator eventAggregator;
     }
 }
