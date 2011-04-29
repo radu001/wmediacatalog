9c549a588a92071959fca87047fcd5057ff1c645
ternovich <ternovich@gmail.com>
2011-04-29 05:35:40 +0000

    update issue #2 Added base infrastructure for artists wasteability functionality

---
+++ Modules.Artists/Converters/ArtistWastedToVisibilityConvreter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using BusinessObjects;
+
+namespace Modules.Artists.Converters
+{
+    public class ArtistWastedToVisibilityConvreter : IValueConverter
+    {
+        public bool Inverse { get; set; }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Artist artist = value as Artist;
+
+            if (artist == null)
+                return Visibility.Collapsed;
+
+            if (!Inverse)
+            {
+                if (artist.IsWaste)
+                    return Visibility.Collapsed;
+
+                return Visibility.Visible;
+            }
+            else
+            {
+                if (artist.IsWaste)
+                    return Visibility.Visible;
+
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
