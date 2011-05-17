c7c90e36383800ba784a61b421a85f6258eabdd1
ternovich <ternovich@gmail.com>
2011-05-17 10:18:19 +0000

    Extracted NHibernate config parsing and handling into common assembly in order to reuse it from db deploy utility

--- Common/Utilities/NHibernate/NHibernateConfigModel.cs
+++ Common/Utilities/NHibernate/NHibernateConfigModel.cs
@@ -4,9 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
-using Common;
 
-namespace DataServices.NHibernate
+namespace Common.Utilities.NHibernate
 {
     public class NHibernateConfigModel : INHibernateConfig
     {
