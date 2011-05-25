45d61e65993502b539860f418530406b2677d695
ternovich <ternovich@gmail.com>
2011-05-25 16:01:39 +0000

    Major update to db deployment tool. Added draft implementation for further improvement

---
+++ Utils/DbTool/DbTool/Data/PsqlScriptTask.cs
@@ -0,0 +1,35 @@
+﻿
+using PsqlDotnet;
+namespace DbTool.Data
+{
+    public class PsqlScriptTask : ScriptTask
+    {
+        public string DbName { get; private set; }
+
+        public IPsqlShell Shell { get; set; }
+
+        public PsqlScriptTask(int index, string name, string description, string script, string dbName)
+            :base(index, name, description, script)
+        {
+            DbName = dbName;
+        }
+
+        public override bool Deploy()
+        {
+            base.Deploy();
+
+            bool success = false;
+
+            try
+            {
+                Shell.ExecuteScript(Script, DbName);
+                success = true;
+            }
+            catch { }
+
+            FinishDeploy(success);
+
+            return success;
+        }
+    }
+}
---
+++ Utils/DbTool/DbTool/Data/ScriptTask.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Common.Data;
+
+namespace DbTool.Data
+{
+    public class ScriptTask : NotificationObject, IDeployTask
+    {
+        public string Script { get; set; }
+
+        public ScriptTask(int index, string name, string description, string script)
+        {
+            Index = index;
+            Name = name;
+            Description = description;
+            Script = script;
+            Status = ItemStatus.Pending;
+        }
+
+        #region IDeployTask Members
+
+        public int Index { get; private set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ItemStatus Status
+        {
+            get
+            {
+                return status;
+            }
+            private set
+            {
+                status = value;
+                NotifyPropertyChanged(() => Status);
+            }
+        }
+
+        public DateTime? StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+            private set
+            {
+                startTime = value;
+                NotifyPropertyChanged(() => StartTime);
+            }
+        }
+
+        public DateTime? EndTime
+        {
+            get
+            {
+                return endTime;
+            }
+            private set
+            {
+                endTime = value;
+                NotifyPropertyChanged(() => EndTime);
+            }
+        }
+
+        public virtual bool Deploy()
+        {
+            StartTime = DateTime.Now;
+            Status = ItemStatus.InProgress;
+            EndTime = null;
+
+            return true;
+        }
+
+        protected void FinishDeploy(bool success)
+        {
+            EndTime = DateTime.Now;
+            Status = success ? ItemStatus.Success : ItemStatus.Error;
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private ItemStatus status;
+        private DateTime? startTime;
+        private DateTime? endTime;
+
+        #endregion
+    }
+}
