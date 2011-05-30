fcc80cfcfe7569e2dc52c2e39f8381a93f96d858
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-05-30 15:08:22 +0000

    Minor changes. Initial step in import data wizard no can be disabled by user

--- trunk/Modules.Import/ViewModels/Wizard/InitialStepViewModel.cs
+++ trunk/Modules.Import/ViewModels/Wizard/InitialStepViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Practices.Prism.Events;
+﻿using BusinessObjects;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Unity;
+using Modules.Import.Services;
 using Modules.Import.ViewModels.Wizard.Common;
 using Prism.Wizards.Events;
 
@@ -7,9 +10,12 @@ namespace Modules.Import.ViewModels.Wizard
 {
     public class InitialStepViewModel : WizardViewModelBase, IInitialStepViewModel
     {
-        public InitialStepViewModel(IUnityContainer container, IEventAggregator eventAggregator)
+        public InitialStepViewModel(IUnityContainer container, IEventAggregator eventAggregator, IDataService dataService)
             : base(container, eventAggregator)
         {
+            this.dataService = dataService;
+
+            ViewLoadedCommand = new DelegateCommand<object>(OnViewLoadedCommand);
         }
 
         #region IInitialStepViewModel Members
@@ -23,11 +29,65 @@ namespace Modules.Import.ViewModels.Wizard
             }
         }
 
+        public bool IsStepDisabledNextTime
+        {
+            get
+            {
+                return isStepDisabledNextTime;
+            }
+            set
+            {
+                isStepDisabledNextTime = value;
+                NotifyPropertyChanged(() => IsStepDisabledNextTime);
+            }
+        }
+
+        public DelegateCommand<object> ViewLoadedCommand { get; private set; }
+
+        #endregion
+
+        #region Private methods
+
+        private void OnViewLoadedCommand(object parameter)
+        {
+            var settings = CurrentUser.GetSettings();
+
+            IsStepDisabledNextTime = !settings.ImportFirstStepVisible;
+
+            if (!settings.ImportFirstStepVisible)
+            {
+                ContinueCommand.Execute(null);
+            }
+        }
+
+        private void ChangeImportDataWizardInitialStepVisibility()
+        {
+            var settings = CurrentUser.GetSettings();
+
+            bool disableStep = !IsStepDisabledNextTime; // inverse
+
+            //setting has been actually changed
+            if (settings.ImportFirstStepVisible != disableStep)
+            {
+                settings.ImportFirstStepVisible = disableStep;
+                dataService.SaveUserSettings();
+            }
+        }
+
         #endregion
 
         public override void OnContinueCommand(object parameter)
         {
+            ChangeImportDataWizardInitialStepVisibility();
+
             eventAggregator.GetEvent<CompleteWizardStepEvent>().Publish(null);
         }
+
+        #region Private fields
+
+        private IDataService dataService;
+        private bool isStepDisabledNextTime;
+
+        #endregion
     }
 }
