528e76ad867ac578035d30ffa6df8e0b0e472251
ternovich <ternovich@gmail.com>
2011-09-07 11:11:23 +0000

    updated issue #35 created database view for tag cloud functionality. Performed cleanup of svn sturcture (added ignores)

---
+++ BusinessObjects/Artificial/TaggedObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessObjects.Artificial
+{
+    public class TaggedObject 
+    {
+        public string Name { get; set; }
+
+        public int ID { get; set; }
+
+        public TaggedObjectType ObjectType { get; set; }
+    }
+}
