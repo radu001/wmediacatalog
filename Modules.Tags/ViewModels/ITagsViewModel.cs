d2141cb58d17a3a9e3e15dffc849035b1aa1cc93
ternovich <ternovich@gmail.com>
2011-09-08 15:46:58 +0000

    update issue #35 improved UI and data layer

--- Modules.Tags/ViewModels/ITagsViewModel.cs
+++ Modules.Tags/ViewModels/ITagsViewModel.cs
@@ -2,19 +2,26 @@
 using System.Collections.ObjectModel;
 using Microsoft.Practices.Prism.Commands;
 using TagCloudLib;
+using BusinessObjects.Artificial;
+using Common.Entities.Pagination;
+using Common.Controls.Controls;
 namespace Modules.Tags.ViewModels
 {
     public interface ITagsViewModel
     {
         ObservableCollection<ITag> Tags { get; }
         ObservableCollection<ITag> SelectedTags { get; }
+        IPagedList<TaggedObject> TaggedObjects { get; }
         ITag AllTagsSelectedItem { get; set; }
         ITag SelectedTagsSelectedItem { get; set; }
+        ILoadOptions LoadOptions { get; }
+        int TaggedObjectsCount { get; }
 
         DelegateCommand<object> ViewLoadedCommand { get; }
         DelegateCommand<object> SelectedTagsDropCommand { get; }
         DelegateCommand<object> AllTagsDropCommand { get; }
         DelegateCommand<object> SelectedTagsDragCommand { get; }
         DelegateCommand<object> AllTagsDragCommand { get; }
+        DelegateCommand<PageChangedArgs> PageChangedCommand { get; }
     }
 }
