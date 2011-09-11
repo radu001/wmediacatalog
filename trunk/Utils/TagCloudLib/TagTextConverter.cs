78afb5fd15c2dd6251177698e53aaf6ad4fb4b04
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-09-11 18:39:42 +0000

    fixed issue #54 when saving artist/album/tag tagsView is refreshed and filtering reapplied

---
+++ trunk/Utils/TagCloudLib/TagTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TagCloudLib
+{
+    public class TagTextConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tag = (ITag)value;
+            return String.Format("{0} ({1})", tag.Name, tag.Rank);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
