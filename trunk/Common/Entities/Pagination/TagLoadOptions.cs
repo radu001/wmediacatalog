2bffb1c5c9b37a7666f26bf9fd3b0f82085832b8
ternovich@gmail.com <ternovich@gmail.com@3d77c6d7-f7de-4e81-fe29-2f85a7effccd>
2011-09-12 18:24:55 +0000

    update issue #57 Update datalayer, stored procedures and schema

--- trunk/Common/Entities/Pagination/TagLoadOptions.cs
+++ trunk/Common/Entities/Pagination/TagLoadOptions.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Common.Entities.Pagination
 {
     public class TagLoadOptions : LoadOptions
     {
-        /// <summary>
-        /// Use *and* concatenation instead of *or* one for filtering entities associated with tags 
-        /// </summary>
-        public bool UseAndConcatenation { get; set; }
-
         public bool ExcludeAlbums { get; set; }
 
         public bool ExcludeArtists { get; set; }
 
-        public bool DistinctByEntityID { get; set; }
+        public string EntityName { get; set; }
     }
 }
